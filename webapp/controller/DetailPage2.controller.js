6c01f060dc934e697f0ed12afa65aad4fe87f4c2
Daniel Serrano <df.serrano@uniandes.edu.co>
2020-05-15 02:47:41 +0000

    Initial Commit. Prototype from build.

---
+++ webapp/controller/DetailPage2.controller.js
@@ -0,0 +1,73 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function (BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.iagMedia.controller.DetailPage2", {
+		handleRouteMatched: function (oEvent) {
+			var sAppId = "App5ea5e6244f3fe474c2a0c14a";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function (oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			if (!this.sContext) {
+				this.sContext = "CustomerSet('1000000000')";
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		avatarInitialsFormatter: function (sTextValue) {
+			return typeof sTextValue === 'string' ? sTextValue.substr(0, 2) : undefined;
+
+		},
+		onInit: function () {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("DetailPage2").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function () {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function () {
+								this.oRouter.navTo("DetailPage1", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+		}
+	});
+}, /* bExport= */ true);
